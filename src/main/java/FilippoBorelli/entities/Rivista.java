bc5183ba291ae32637e920305b53b0a76a524d7f
Filippo2803 <filippoborelli8@gmail.com>
2024-03-15 13:10:04 +0100

    add Rivista class

---
+++ src/main/java/FilippoBorelli/entities/Rivista.java
@@ -0,0 +1,24 @@
+package FilippoBorelli.entities;
+
+import FilippoBorelli.entities.Articolo;
+
+public class Rivista extends Articolo {
+
+    public Periodicita periodicita;
+
+    public Rivista(String codiceIsbn, String titolo, int annoPubblicazione, int numeroPagine, Periodicita periodicita) {
+        super(codiceIsbn, titolo, annoPubblicazione, numeroPagine);
+
+        this.periodicita = periodicita;
+    }
+    @Override
+    public String toString() {
+        return "Rivista{" +
+                "codiceIsbn='" + codiceIsbn + '\'' +
+                ", titolo='" + titolo + '\'' +
+                ", annoPubblicazione=" + annoPubblicazione +
+                ", numeroPagine=" + numeroPagine +
+                ", periodicita='" + periodicita + '\'' +
+                '}';
+    }
+}
